341275aa59f015c2f03cc9a042e3ac1797781e50
Tuna Görevin <65346998+at-max@users.noreply.github.com>
2023-04-23 10:02:17 +0300

    Debian türevleri için

---
+++ debcus.py
@@ -0,0 +1,88 @@
+import subprocess
+import os
+
+def show_menu():
+    print("1. LXDE kur")
+    print("2. KDE Plasma kur")
+    print("3. MATE kur")
+    print("4. Cinnamon kur")
+    print("5. Openbox kur")
+    choice = input("Seçiminizi girin: ")
+    return choice
+
+def install_lxde():
+    subprocess.run(['sudo', 'apt-get', 'update'])
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'lxde'])
+
+def install_kde_plasma():
+    subprocess.run(['sudo', 'apt-get', 'update'])
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'kde-plasma-desktop'])
+
+def install_mate():
+    subprocess.run(['sudo', 'apt-get', 'update'])
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'mate-desktop'])
+
+def install_cinnamon():
+    subprocess.run(['sudo', 'apt-get', 'update'])
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'cinnamon'])
+
+def install_openbox():
+    subprocess.run(['sudo', 'apt-get', 'update'])
+    subprocess.run(['sudo', 'apt-get', 'install', '-y', 'openbox'])
+    install_openbox_panels()
+
+def install_openbox_panels():
+    print("Lütfen bir panel seçin: ")
+    print("1. Tint2")
+    print("2. LXPanel")
+    panel_choice = input("Seçiminizi girin: ")
+    if panel_choice == "1":
+        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'tint2'])
+    elif panel_choice == "2":
+        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'lxpanel'])
+    else:
+        print("Geçersiz seçim.")
+
+choice = show_menu()
+
+if choice == "1":
+    install_lxde()
+    web()
+elif choice == "2":
+    install_kde_plasma()
+    web()
+elif choice == "3":
+    install_mate()
+    web()
+elif choice == "4":
+    install_cinnamon()
+    web()
+elif choice == "5":
+    install_openbox()
+    web()
+else:
+    print("Geçersiz seçim.")
+
+def web():
+    print("Web tarayıcısı seçiniz:")
+    print("1. Google Chrome")
+    print("2. Mozilla Firefox")
+    print("3. Opera")
+
+    # Kullanıcı seçimini al
+    secim = input("Seçiminiz (1/2/3): ")
+
+    if secim == "1":
+        # Google Chrome'u indir ve kur
+        os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+        os.system("sudo dpkg -i google-chrome-stable_current_amd64.deb")
+    elif secim == "2":
+        # Mozilla Firefox'u kur
+        os.system("sudo apt-get update")
+        os.system("sudo apt-get install firefox")
+    elif secim == "3":
+        # Opera'yı indir ve kur
+        os.system("wget https://download3.operacdn.com/pub/opera/desktop/80.0.4170.16/linux/opera-stable_80.0.4170.16_amd64.deb")
+        os.system("sudo dpkg -i opera-stable_80.0.4170.16_amd64.deb")
+    else:
+        print("Geçersiz seçim!")
